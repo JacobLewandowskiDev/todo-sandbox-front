27bfa4638ac9487d91acbd9586c6056b7aa882bf
Jacob <lewandowskiJakub9@gmail.com>
2024-02-08 21:49:54 -0500

    Main page wip

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,11 +1,66 @@
-<template>
-
-</template>
-
-<script>
-
+<script setup>
 </script>
 
-<style>
+<template>
+    <div id="container">
+        <h1>Todos</h1>
+        <div class="todo-list">
+            <h2>No Todos</h2>
+            <p>Get started by creating a new todo</p>
+        </div>
+        <br>
+        <button @click=""><span id="plus">+</span> New Todo</button>
+    </div>
+    
+</template>
 
+<style>
+#container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+h1 {
+    position: relative; 
+    right: 110px;
+}
+
+.todo-list {
+    border: dashed 0.3em #848484;
+    width: 20em;
+    height: 20em;
+    border-radius: 10px;
+    padding: 10px;
+    text-align: center;
+}
+
+button {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #2e43ff;
+    color: #ffffff;
+    font-size: 1.5em;
+    line-height: 2em;
+    text-align: center;
+    padding: 0 2em;
+    font-weight: bold;
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    border: none;
+    border-radius: .3em;
+    cursor: pointer;
+    transition-duration: 0.4s;
+}
+
+button:hover {
+  background-color: #2237f3;
+  color: #ffffff;
+}
+
+button span {
+    font-size:1.8em;
+    font-weight: initial;
+    margin-right: 0.2em;
+}
 </style>
