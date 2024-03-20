cfd58a023825451b1595467b48d1fcdcd4c30602
Jacob <lewandowskiJakub9@gmail.com>
2024-03-19 22:12:39 -0400

    This commit adds the last functionality to update steps of existing todos via API calls

--- src/components/SubtaskList.vue
+++ src/components/SubtaskList.vue
@@ -49,7 +49,33 @@ import { apiUrl } from '../router/index.js'
                 }
             },
 
-            handleUpdateStep(index) {
+            async handleUpdateStep(index) {
+                const originalStep = { ...this.steps[index] };
+                const updatedStep = this.steps[index];
+                try {
+                    if(this.todoId != null) {
+                        const response = await fetch(`${apiUrl}/todos/${this.todoId}/steps`, {
+                            method: 'PUT',
+                            headers: {
+                                'Content-type': 'application/json'
+                            },
+                            body: JSON.stringify(updatedStep)
+                        });
+                        if(response.ok) {
+                            console.log("Update of the step was successfull.");
+                            this.$router.push({name: 'Details'});
+                        } else {
+                            this.steps[index] = originalStep;
+                            alert('Failed to update the step for existing todo. Please check your inputs');
+                            throw new Error('Failed to update the step for existing todo.');
+                        }
+                    }
+                } catch(error) {
+                    console.log("Failed to update step: " + error);
+                }
+            },
+
+            handleUpdateStepLocally(index) {
                 this.$emit('updateStepLocally', index, this.steps[index]);
             }
         }
@@ -61,11 +87,11 @@ import { apiUrl } from '../router/index.js'
     <br>
     <div class="subtasklist" :class="{ 'empty-subtasks': steps.length === 0}">
         <div v-for="(step, index) in steps" :key="index" class="subtasklist__subtask">
-            <input class="subtasklist__subtask__input" type="text" v-model="step.name" :placeholder="'Name: ' + step.name" maxlength="100" @input="handleUpdateStep(index)">
+            <input class="subtasklist__subtask__input" type="text" v-model="step.name" :placeholder="'Name: ' + step.name" maxlength="100" @input="handleUpdateStepLocally(index)">
             <br>
-            <input class="subtasklist__subtask__input" type="text" v-model="step.description" :placeholder="'Description: ' + step.description" maxlength="3000" @input="handleUpdateStep(index)">
+            <input class="subtasklist__subtask__input" type="text" v-model="step.description" :placeholder="'Description: ' + step.description" maxlength="3000" @input="handleUpdateStepLocally(index)">
             <div class="subtasklist__subtask__container">
-                <button class="subtasklist__subtask__container--update" v-if="showUpdateButton" @click="handleUpdateStep(index, { step })" type="button">Update Subtask</button>
+                <button class="subtasklist__subtask__container--update" v-if="showUpdateButton" @click="handleUpdateStep(index)" type="button">Update Subtask</button>
                 <button class="subtasklist__subtask__container--delete" @click="handleDeleteStep(index)" type="button">Delete Subtask</button>
             </div>
             <br class="form__break">
