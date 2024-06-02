7b3d4cc8932b74d003474d4231b33a8de2d65f6b
Jacob <lewandowskiJakub9@gmail.com>
2024-06-02 19:18:40 -0400

    This commit makes minor updates

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import TodoList from "../views/TodoList.vue";
 import CreateTodo from "../views/CreateTodo.vue";
 import TodoDetails from "../components/TodoDetails.vue";
 
-export const apiUrl = "https://www.jakub-lewandowski.com:2052";
+export const apiUrl = "https://www.jakub-lewandowski.com/vpt/api";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
